1b537acdefb99450284569f2684cbc42640e5e26
Jacob Babbitt <jacobhbabbitt@gmail.com>
2022-12-12 09:31:38 -0600

    initial commit

---
+++ main.js
@@ -0,0 +1,24 @@
+const add = (firstNumber, secondNumber) => {
+    const sum = firstNumber + secondNumber
+    return sum
+}
+const subtract = (firstNumber, secondNumber) => {
+    const idkWhatCalled = firstNumber - secondNumber
+    return idkWhatCalled
+}
+const multiply = (firstNumber, secondNumber) => {
+    const multipliedNumber = firstNumber * secondNumber
+    return multipliedNumber
+}
+const divide = (firstNumber, secondNumber) => {
+    const qotient = firstNumber / secondNumber
+    return qotient
+}
+const square = (firstNumber) => {
+    const squared = firstNumber * firstNumber
+    return squared
+}
+// (0.25x * 4) + (o.75x * 2) = 60
+// x + 1.5x = 60
+// 2.5 x = 60
+//24
